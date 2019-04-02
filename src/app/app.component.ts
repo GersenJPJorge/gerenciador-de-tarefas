bf5f0032defcabc8812fcd0c8dd1b3efbd80b8f2
Gersen James <gersen.jorge@gmail.com>
2019-04-02 17:40:20 -0300

    Instalando o bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gerenciador-fe-tarefas';
+  title = 'Gerenciador de tarefas';
 }
