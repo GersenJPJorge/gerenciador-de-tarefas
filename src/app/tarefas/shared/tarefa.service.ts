3153455929007c1e38cb71cd7ee76d99216fac13
Gersen James <gersen.jorge@gmail.com>
2019-04-03 11:15:01 -0300

    Criando o serviço de gerenciamento de tarefas 2/2

--- src/app/tarefas/shared/tarefa.service.ts
+++ src/app/tarefas/shared/tarefa.service.ts
@@ -8,7 +8,7 @@ export class TarefaService {
 
   constructor() { }
 
-  listarTodos(): Tarefa[] {
+listarTodos(): Tarefa[] {
     // o localstorage é um array do tipo chave,valor que está disponível pelo navegador
     // o const  serve para variáveis ou constantes que não serão modificadas ao longo do código
   const tarefas = localStorage['tarefas'];
@@ -20,7 +20,7 @@ export class TarefaService {
   }
 
   // é void porque não retorna nada
-  cadastrar(tarefa: Tarefa): void {
+cadastrar(tarefa: Tarefa): void {
   	const tarefas = this.listarTodos();
   	tarefa.id = new Date().getTime();
     tarefas.push(tarefa);
@@ -29,42 +29,48 @@ export class TarefaService {
     // fazendo ao contrario, pegando do json e jogando em localstorage
   }
 
-  /*
-  buscarPorId(id: number): Tarefa {
+buscarPorId(id: number): Tarefa {
     const tarefas: Tarefa[] = this.listarTodos();
     return tarefas.find(tarefa => tarefa.id === id);
-  }
-*/
+// o find é um utilitário(método) do javascript que faz uma iteração sobre todas as tarefas,
+// e para cada tarefa ele verifica se o id da tarefa que  estou iterando é igual ao id que estou procurando  
+}
 
-  /*
-  atualizar(tarefa: Tarefa): void {
+atualizar(tarefa: Tarefa): void {
     const tarefas: Tarefa[] = this.listarTodos();
     tarefas.forEach((obj, index, objs) => { 
+    // obj é a tarefa em si
+    // posicao da tarefa sendo iterada no momento
+    // objs é uma lista das tarefas(é o tarefas do foreach)  
       if (tarefa.id === obj.id) {
+        // e o id da tarefa for igual ao id na qual estou iterando  
         objs[index] = tarefa;
       }
     });
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
-*/
 
-  /*
-  remover(id: number): void {
+
+remover(id: number): void {
     let tarefas: Tarefa[] = this.listarTodos();
+    // usou o let porque após a exclusão o tarefas terá que ser atribuido com uma nova lista de taferas
     tarefas = tarefas.filter(tarefa => tarefa.id !== id);
+    // com o filter retorna todas menos a que eu estou iterando gerando uma nova só com as que não serão removidas
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
-*/
 
-  /*
-  alterarStatus(id: number): void {
+
+alterarStatus(id: number): void {
     const tarefas: Tarefa[] = this.listarTodos();
     tarefas.forEach((obj, index, objs) => { 
       if (id === obj.id) {
         objs[index].concluida = !obj.concluida;
+        // concluida está no constructor de tarefas como booleano(tarefas.model)
+        // para atualizar é só negar a condição
+        // substitui somente o objeto concluida, não precisa ser a lista toda (é só um checkbox)
       }
     });
     localStorage['tarefas'] = JSON.stringify(tarefas);
   }
-*/
+
 }
