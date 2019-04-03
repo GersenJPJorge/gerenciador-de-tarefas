6ae314db8603a826000fb0aaa848ae767a5c10a4
Gersen James <gersen.jorge@gmail.com>
2019-04-03 17:02:50 -0300

    Registrando as rotas de edição de tarefas

--- src/app/tarefas/tarefas-routing.module.ts
+++ src/app/tarefas/tarefas-routing.module.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { ListarTarefaComponent } from './listar';
 import { CadastrarTarefaComponent } from './cadastrar';
+import { EditarTarefaComponent } from './editar';
 
 export const TarefaRoutes: Routes = [
 	{ 
@@ -18,10 +19,11 @@ export const TarefaRoutes: Routes = [
 		component: CadastrarTarefaComponent 
 	},
 
+	{ 
+		path: 'tarefas/editar/:id', 
+		component: EditarTarefaComponent 
+	}
+
 ];
 
-//	{ 
-//		path: 'tarefas/editar/:id', 
-//		component: EditarTarefaComponent 
-//	}
 
