4446071c131f1b136db558058107fcfafa69dd67
Gersen James <gersen.jorge@gmail.com>
2019-04-03 11:53:02 -0300

    Registrando as rotas de listagem de tarefas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TarefaRoutes } from './tarefas';
 
 export const routes: Routes = [
+	{ 
+		path: '',                                   // na inicialização ou se não for digitado nada na url
+		redirectTo: '/tarefas/listar', 
+		pathMatch: 'full'                           // passa a url completa
+	},
+	...TarefaRoutes                               // recurso do typescript que permite concatenar arrays
 ];
+
 
 @NgModule({
   imports: [ RouterModule.forRoot(routes) ],  // forRoot indica que ele é único, que controlará todos as rotas da aplicação
