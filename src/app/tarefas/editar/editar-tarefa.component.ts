b3216c08adb3700e30b38bd9a90fecb66ac8c83a
Gersen James <gersen.jorge@gmail.com>
2019-04-03 17:48:44 -0300

    Implementando a edição de tarefas

--- src/app/tarefas/editar/editar-tarefa.component.ts
+++ src/app/tarefas/editar/editar-tarefa.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router'; 
+import { NgForm } from '@angular/forms'; 
+
+import { TarefaService, Tarefa } from '../shared';
 
 @Component({
   selector: 'app-editar-tarefa',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditarTarefaComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('formTarefa') formTarefa: NgForm;
+  tarefa: Tarefa;
+
+  constructor(
+            private tarefaService: TarefaService,
+  	        private route: ActivatedRoute,
+  	        private router: Router) {}
 
   ngOnInit() {
+    const id = +this.route.snapshot.params['id'];
+    // atraves do módulo route, do ActivatedRoute, temos acesso ao objeto snapshot seguido de params.
+    // em params temos todos os parametros contidos na url seguido de ':'
+    // no nosso caso é :id, portanto basta chamar por id
+    // o +this é um operador do typscript que converte o valor id em formato string para número, garantindo nçao ter dado inválido na url 
+	this.tarefa = this.tarefaService.buscarPorId(id);
+  }
+
+  atualizar(): void {
+    if (this.formTarefa.form.valid) {
+      this.tarefaService.atualizar(this.tarefa);
+      // atualizou, vai para tarefas
+	    this.router.navigate(['/tarefas']);
+    }
   }
 
 }
